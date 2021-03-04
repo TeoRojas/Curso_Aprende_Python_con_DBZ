63ac84534c675771d2b9f35636bcd3a4f8a3f586
TeoRojas <teofilo.rojas.mata@gmail.com>
2021-03-04 20:38:41 +0100

    nuevos archivos de variables

---
+++ Variables/VAR_numeros_01_01.py
@@ -0,0 +1,11 @@
+variable_valor_entero_positivo = 33
+variable_valor_entero_negativo = -10
+variable_valor_flotante_positivo = 1.5
+variable_valor_flotante_negativo = -3.1
+
+print("Variable entera positiva - Variable entera negativa =")
+print(variable_valor_entero_positivo + variable_valor_entero_negativo)
+print("Variable flotante positiva - Variable flotante negativa =")
+print(variable_valor_flotante_positivo - variable_valor_flotante_negativo)
+print("Variable entera negativa - Variable flotante negativa =")
+print(variable_valor_entero_negativo - variable_valor_flotante_negativo)
