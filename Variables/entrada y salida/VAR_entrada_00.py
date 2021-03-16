283ed488df38214dc57a8ef6cf9395c9c891e4ed
TeoRojas <teofilo.rojas.mata@gmail.com>
2021-03-16 11:44:10 +0100

    organización en carpetas

---
+++ Variables/entrada y salida/VAR_entrada_00.py
@@ -0,0 +1,11 @@
+#Tu primer ataque de energía
+tu_nombre = input("¿Cuál sería tu nombre si fueses Saiyajin?\n")
+tu_ataque_de_energia = input("¿Cómo llamarás a tu ataque de energía?\n")
+
+print("...\n"
+    + "Hola " 
+    + tu_nombre 
+    + "!\n"
+    + "Hoy vas a entrenar '"
+    + tu_ataque_de_energia
+    + "'. Así que... ¡eleva tu Ki!.")
